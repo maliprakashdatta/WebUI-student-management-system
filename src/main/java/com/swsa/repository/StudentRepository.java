fe11da4e4f353c27aced93f1dfb8776b7f63ae6c
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-30 19:29:08 +0530

    committed

--- src/main/java/com/swsa/repository/StudentRepository.java
+++ src/main/java/com/swsa/repository/StudentRepository.java
@@ -7,23 +7,37 @@ import java.util.List;
 
 public class StudentRepository {
 
-    private static Connection connection=null;
-/*
-    //private  Connection connection;
-        public StudentRepository() {
-            try {
-                // Assuming you're using MySQL
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystems", "root", "Prakash@123");
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-            }
+    private static Connection connection = null;
 
-        }*/
     private void initConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             connection = new ConnectionService().getConnection();
         }
+    }
+
+
+
+    public boolean insertStudent(Student student) throws SQLException {
+        this.initConnection();
+
+        String query = "INSERT INTO student VALUES(?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, student.getId());
+            preparedStatement.setString(2, student.getFirstName());
+            preparedStatement.setString(3, student.getLastName());
+            preparedStatement.setString(4, student.getGender());
+            preparedStatement.setInt(5, student.getAge());
+
+            System.out.println("inserting student object to student table: " + student);
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
     }
 
 
@@ -43,7 +57,7 @@ public class StudentRepository {
                 String lastName = resultSet.getString("lastname");
                 String gender = resultSet.getString("gender");
                 int age = resultSet.getInt("age");
-                Student student = new Student(id,firstName, lastName, gender, age);
+                Student student = new Student(id, firstName, lastName, gender, age);
                 students.add(student);
             }
         } catch (SQLException e) {
@@ -62,30 +76,5 @@ public class StudentRepository {
     }
 
 
-    public boolean insertStudent(Student student) throws SQLException {
-        this.initConnection();
-
-        String query = "INSERT INTO student VALUES(?, ?, ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
-            preparedStatement.setInt(1, student.getId());
-            preparedStatement.setString(2, student.getFirstName());
-            preparedStatement.setString(3, student.getLastName());
-            preparedStatement.setString(4, student.getGender());
-            preparedStatement.setInt(5, student.getAge());
-
-            System.out.println("inserting student object to student table: " + student);
-            int rowsInserted = preparedStatement.executeUpdate();
-
-            return rowsInserted > 0;
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-        return false;
-
-    }
-
-
 }
+
